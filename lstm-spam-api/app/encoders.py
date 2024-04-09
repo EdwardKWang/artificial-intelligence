d6bbaa0ede091ed9d016013e37e1bbccf965f2df
EdwardKWang <widener-telcos.0d@icloud.com>
2024-04-08 20:50:51 -0400

    Refactor main.py to improve AI model loading and prediction

---
+++ lstm-spam-api/app/encoders.py
@@ -0,0 +1,23 @@
+import json
+import numpy as np
+
+class NumpyEncoder(json.JSONEncoder):
+    """This is a custom JSONEncoder for numpy types
+
+    :param json: _description_
+    :type json: _type_
+    """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+    
+def encode_to_json(data, as_py=True):
+    encoded = json.dumps(data, cls=NumpyEncoder)
+    if as_py:
+        encoded = json.loads(encoded)
+    return encoded
--- lstm-spam-api/app/main.py
+++ lstm-spam-api/app/main.py
@@ -1,19 +1,11 @@
-"""_summary_
-
-:return: _description_
-:rtype: _type_
-"""
-
+#This is the main module for the FastAPI application
 # uvicorn app.main:app --reload
 
 import pathlib
-import json
-import numpy as np
 from typing import Optional
 from fastapi import FastAPI
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import tokenizer_from_json
+
+from . import ml
 
 app = FastAPI()
 
@@ -25,55 +17,17 @@ TOKENIZER_PATH = SMS_SPAM_MODEL_DIR / "spam-classifier-tokenizer.json"
 METADATA_PATH = SMS_SPAM_MODEL_DIR / "spam-classifier-metadata.json"
 
 AI_MODEL = None
-AI_TOKENIZER = None
-MODEL_METADATA = {}
-labels_legend_inverted = {}
-
-class NumpyEncoder(json.JSONEncoder):
-    """Encoder to handle integer, float and numpy array types
-
-    :param json: object to encode
-    :type json: json object
-    """
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        if isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
 
 @app.on_event("startup")
 def on_startup():
-    """startup event handler
+    """Startup event handler
     """
-    global AI_MODEL, AI_TOKENIZER, MODEL_METADATA, labels_legend_inverted
-    # load model
-    if MODEL_PATH.exists():
-        AI_MODEL = load_model(MODEL_PATH)
-    # load tokenizer
-    if TOKENIZER_PATH.exists():
-        with open(TOKENIZER_PATH, "r") as f:
-            AI_TOKENIZER = tokenizer_from_json(f.read())
-    # load metadata
-    if METADATA_PATH.exists():
-        with open(METADATA_PATH, "r") as f:
-            MODEL_METADATA = json.load(f)
-            labels_legend_inverted = MODEL_METADATA["labels_legend_inverted"]
-
-def predict(query:str):
-    sequences = AI_TOKENIZER.texts_to_sequences([query])
-    maxlen = MODEL_METADATA.get("max_sequence") or 280
-    x_input = pad_sequences(sequences, maxlen=maxlen)
-    preds_array = AI_MODEL.predict(x_input)
-    preds = preds_array[0]
-    top_idx_val = np.argmax(preds)
-    top_pred = {"label": labels_legend_inverted[str(top_idx_val)],
-                "confidence": preds[top_idx_val]}
-    labeled_preds = [{"label": labels_legend_inverted[str(i)],
-                      "confidence": x} for i , x in enumerate(list(preds))]
-    return json.loads(json.dumps({"top": top_pred, "predictions": labeled_preds}, cls=NumpyEncoder))
+    global AI_MODEL
+    AI_MODEL = ml.AIModel(
+        model_path=MODEL_PATH,
+        tokenizer_path=TOKENIZER_PATH,
+        metadata_path=METADATA_PATH
+    )
 
 @app.get("/") # /?q=this is awesome
 def read_index(q:Optional[str] = None):
@@ -84,7 +38,7 @@ def read_index(q:Optional[str] = None):
     :return: _description_
     :rtype: _type_
     """
-    global AI_MODEL, MODEL_METADATA, labels_legend_inverted
+    global AI_MODEL
     query = q or "Hello World"
-    preds_dict = predict(query)
+    preds_dict = AI_MODEL.predict_text(query)
     return {"query": query, "results":preds_dict}
